9df557e4f57c58f0fd1c3d5bc77609fcaa3efc95
Fetandrey <fetandrey@outlook.com>
2016-09-23 12:47:04 +0300

    Initial menu implementation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Shift");
+            Console.WriteLine("2. Pairs");
+            int desicion = Convert.ToInt32(Console.ReadLine());
+            switch(desicion)
+            {
+                case 1:
+                    Shift();
+                    break;
+                case 2:
+                    Pairs();
+                    break;
+                default:
+                    break;
+            }
+        }
+        private static void Shift()
+        {
+            Console.WriteLine("Shift chosen");
+            Console.ReadLine();
+        }
+        private static void Pairs()
+        {
+            Console.WriteLine("Pairs chosen");
+            Console.ReadLine();
         }
     }
 }
